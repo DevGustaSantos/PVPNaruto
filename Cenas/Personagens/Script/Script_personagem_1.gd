82e23e6cb5a319b264ef60b7574f60ad6bb477fd
Gustavo Santos <gustavosk.a@hotmail.com>
2021-09-12 18:51:19 -0300

    Func_Controle_de_animacao
    Foi implementado uma funcao para verificar e selecionar a animacao adquada.

--- Cenas/Personagens/Script/Script_personagem_1.gd
+++ Cenas/Personagens/Script/Script_personagem_1.gd
@@ -1,18 +1,27 @@
 extends KinematicBody2D
 
 #---- INICIO VARIAVEIS ----#
-#Controle para movimentação.
-var movimento = Vector2.ZERO
+#Gusta: Controle para movimentação.
+var movimento = Vector2(0,0)
 export var veloc_mov = 480
 export var forca_pulo = -720
 #------ FIM VARIAVEIS ------#
 
 #---- INICIO FUNÇÕES ----#
 func _movimento_personagem():
-	#Realizar o movimento para esquerda(-x) e para a direita(+x)
+	#Gusta: Realizar o movimento para esquerda(-x) e para a direita(+x)
 	var move_direcoes = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
 	movimento.x = veloc_mov * move_direcoes
 	move_and_slide(movimento)
+
+func _controle_animacao():
+	if movimento.x == 0:
+		$anim.play("parado")
+	else:
+		$anim.play("andando")
+
+
 func _physics_process(delta) -> void:
+	_controle_animacao()
 	_movimento_personagem()
 #------ FIM FUNÇÕES ------#
