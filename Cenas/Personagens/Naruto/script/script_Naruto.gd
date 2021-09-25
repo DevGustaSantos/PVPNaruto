bb91b70256ede6a0ce0a44928cee672f44d79ee4
SwordAway <johenrrique20@gmail.com>
2021-09-24 21:44:15 -0300

    Sequencia_atk_falta_terceiro_atk
    O esquema de sequencia e quebrar barreira junto de outras animçãoes ja estão prontas, mas precisa reformular a sequencia de ataque pois está dando conflitos, GU LEMBRA DO TIMER, VERIFICAÇÃO E EOS CARAI, TAMO NO CAMINHO CERTO

--- Cenas/Personagens/Naruto/script/script_Naruto.gd
+++ Cenas/Personagens/Naruto/script/script_Naruto.gd
@@ -3,19 +3,29 @@ extends PersonagemGenerico
 var run = false
 var ataque = false
 var ataque_clone = preload("res://Cenas/Personagens/Naruto/Ataque_clone.tscn")
-
+var qtd_socos = 0
+var socos = false
+var combos = false
 
 func _ready():
 	self.Velocidade_de_movimento = 200 
 	run = false
 	ataque = false
-	
+	socos = false
+	combos = false
+
+func _combos():
+	for i in range(3):
+		
+		pass
+
+
+
 func _animacoes_basicas(delta):
 	
 #	print(self.Velocidade_de_movimento)
 	if (not ataque):
-		
-	#	Controle de correr, andar e parar
+	#	-----Controle de correr, andar e parar-----
 		if(Input.is_action_pressed("correr")):
 			run = true
 		else: 
@@ -32,14 +42,14 @@ func _animacoes_basicas(delta):
 		elif not $Anima.current_animation == "pulando" && is_on_floor():
 			$Anima.play("parado")
 			
-	#	Controle de cair e pular
+	#	-----Controle de cair e pular-----
 		if is_on_floor() && Input.is_action_just_pressed("ui_up"):
 			$Anima.play("pulando")
 			
 		elif not is_on_floor() && not $Anima.current_animation == "pulando":
 			$Anima.play("caindo")
 			
-#			Habilidades e Especiais
+#			-----Jutsu clone-----
 		if Input.is_action_just_pressed("jutsu_clone"):
 			$fum.visible = true
 			$Anima.play("jutsu_clone")
@@ -53,12 +63,46 @@ func _animacoes_basicas(delta):
 			if(self.Controle_do_giro == false):
 				clone.scale.x = -1
 			get_tree().root.add_child(clone)
+			
+#			-----Quebra defesa-----
+		if Input.is_action_just_pressed("quebra_defesa"):
+			$Anima.play("quebra_defesa")
+			ataque = true
+			
+#			-----Inicio dos combos-----
+		elif Input.is_action_just_pressed("combo") and is_on_floor():
+			self.Velocidade_de_movimento = 0
+			$Anima.play("soco")
+			ataque = true
+			
+	
 	else:
 		
+		if Input.is_action_just_pressed("combo") and is_on_floor():
+				self.Velocidade_de_movimento = 0
+				$Anima.play("cabecada")
+				combos = true
+				
+		if(combos):
+	#		Sistemas de combos
+			if Input.is_action_just_pressed("combo") and is_on_floor() and not $Anima.current_animation == "cabecada":
+				self.Velocidade_de_movimento = 0
+				$Anima.play("socao")
+				
+		else:
+			
 			pass
+			
+#			
+#	elif(socos):
+#		if Input.is_action_just_pressed("combo") and is_on_floor():
+#			self.Velocidade_de_movimento = 0
+#			$Anima.play("socao")
+#			pass
 	
 func _physics_process(delta):
 	_animacoes_basicas(delta)
+	_combos()
 	pass
 	
 
@@ -69,8 +113,24 @@ func _on_Anima_animation_finished(anim_name):
 		$Anima.play("caindo")
 		
 # Controlando a animação de atacar
-	if anim_name == "jutsu_clone" || anim_name == "fumaca":
+	if (anim_name == "jutsu_clone" or anim_name == "fumaca" or anim_name == "quebra_defesa"
+	or anim_name == "soco" or anim_name == "cabecada"):
 		$fum.visible = false
 		self.Velocidade_de_movimento = 200
 		ataque = false
-
+	if(anim_name == "socao"):
+		$fum.visible = false
+		self.Velocidade_de_movimento = 200
+		ataque = false
+		combos = false
+		
+# Controlando o combo
+#	if(anim_name == "soco" or anim_name == "cabecada" ):
+#		self.Velocidade_de_movimento = 200
+#		qtd_socos = 0
+#		ataque = false
+		
+#	if (anim_name == "socao"):
+#		ataque = false
+#		socos = false
+		pass
