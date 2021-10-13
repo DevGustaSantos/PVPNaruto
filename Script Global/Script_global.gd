faab34429c42af10292f15d1883c0e529a47775a
Gustavo Santos <gustavosk.a@hotmail.com>
2021-10-13 19:11:50 -0300

    Criacao_Cliente_Servidor
    Criacao_Cliente_Servidor

--- Script Global/Script_global.gd
+++ Script Global/Script_global.gd
@@ -1,5 +1,9 @@
 extends Node
 class_name RegrasDoMundo
 
-#---- Variaveis ----#
+#---- Inicio Variaveis do jogo ----#
 var Gravidade = 1200
+#---- Fim Variaveis do jogo ----#
+
+#---- Inicio Variaveis Multiplayer ----#
+var player2_id = -1
