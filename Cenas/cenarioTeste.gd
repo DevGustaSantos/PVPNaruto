9a0bf55c3bc6be553b7aefc0b45b29d1569770d3
Gustavo Santos <gustavosk.a@hotmail.com>
2021-10-17 20:05:12 -0300

    Spawn_personagens_em_cenario
    Spawn_personagens_em_cenario

--- Cenas/cenarioTeste.gd
+++ Cenas/cenarioTeste.gd
@@ -1,9 +1,21 @@
 extends Node2D
 
+const cenaPlayer = "res://Cenas/Personagens/Naruto/Naruto_personagem.tscn"
+
+onready var player1pos = $Player1Pos
+onready var player2pos = $Player2Pos
+
 func _ready():
-	var new_player = preload("res://Cenas/Personagens/Naruto/Naruto_personagem.tscn").instance()
-	new_player.name = str(get_tree().get_network_unique_id())
-	new_player.set_network_master(get_tree().get_network_unique_id())
-	get_tree().get_root().add_child(new_player)
-	var info = Network.self_data
-	new_player.init(info.name, info.position, false)
+	#CONFIGURAÇÕES PLAYER 1
+	var player1 = preload(cenaPlayer).instance()
+	player1.set_name(str(get_tree().get_network_unique_id()))
+	player1.set_network_master(get_tree().get_network_unique_id())
+	player1.global_transform = player1pos.global_transform
+	add_child(player1)
+	
+	#CONFIGURAÇÕES PLAYER 2
+	var player2 = preload(cenaPlayer).instance()
+	player2.set_name(str(Global.player2_id))
+	player2.set_network_master(Global.player2_id)
+	player2.global_transform = player2pos.global_transform
+	add_child(player2)
