420e3f32290bd35b158f59f7cc923a6259819c6b
SwordAway <johenrrique20@gmail.com>
2021-10-29 18:58:24 -0300

    Att
    Mini HUD

--- Script Global/Script_global.gd
+++ Script Global/Script_global.gd
@@ -3,3 +3,12 @@ class_name RegrasDoMundo
 
 #---- Variaveis ----#
 var Gravidade = 1200
+var Vida_player = 100
+var Chakra_player = 40
+
+func _process(delta):
+	if(Vida_player > 100):
+		Vida_player = 100
+	if(Chakra_player > 40):
+		Chakra_player = 40
+	
