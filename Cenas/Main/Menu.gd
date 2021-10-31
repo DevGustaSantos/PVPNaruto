9cd5aec4adf27a275a8837841929169f8c080d28
Cesarplayet <58156085+Cesarplayet@users.noreply.github.com>
2021-10-31 16:09:00 -0300

    New Features

--- Cenas/Main/Menu.gd
+++ Cenas/Main/Menu.gd
@@ -1,11 +1,7 @@
 extends Control
-
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
+var Registerlink = "www.facebook.com"
+var CreditsLink = "http://www.republiquedesmangues.fr"
+var JoinLink = "http://www.omfgdogs.com"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$BackGroundMain.playing = true
@@ -25,3 +21,23 @@ func _on_CreditsButton_mouse_entered():
 
 func _on_CreditsButton_mouse_exited():
 	$CreditsButton/AnimatedSprite.playing = false
+
+
+func _on_RegisterButton_mouse_entered():
+	$RegisterButton/AnimatedRegister.playing = true
+
+
+func _on_RegisterButton_mouse_exited():
+	$RegisterButton/AnimatedRegister.playing = false
+
+
+func _on_RegisterButton_pressed():
+	OS.shell_open(Registerlink)
+
+
+func _on_CreditsButton_pressed():
+	OS.shell_open(CreditsLink)
+
+
+func _on_Join_pressed():
+	OS.shell_open(JoinLink)
